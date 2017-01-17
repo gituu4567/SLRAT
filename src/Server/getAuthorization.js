fa68fac0b73cf6ba18db84a194c1ad2a883562d4
Liu Shuo <liu@shuo.email>
2017-01-17 13:39:24 +0800

    will redirect back to client

--- src/Server/getAuthorization.js
+++ src/Server/getAuthorization.js
@@ -1,7 +1,7 @@
 const base32 = require('base32')
 
 function getAuthorization (request, response) {
-  if (!request.session.user) return response.redirect(302, '/login')
+  if (!request.session.user) return response.redirect(302, `/login${request._parsedUrl.search}`)
   if (request.session.user) {
     let timestamp = new Date().valueOf()
     let username = request.session.user
