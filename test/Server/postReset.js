748294c237ea9c1539dbab6393fce06f67ee30ba
Liu Shuo <liu@shuo.email>
2017-02-03 13:39:42 +0800

    add postReset 401 test

--- test/Server/postReset.js
+++ test/Server/postReset.js
@@ -1,5 +1,48 @@
 /* eslint-env mocha */
+const assert = require('assert')
+const request = require('request-promise')
+import Server from '../../src/Server/main.js'
+
+import config from './config.js'
 describe('postReset', () => {
-  it('should respond 401 if no email is matched')
-  it('should respond 200 if resetcode is sent to email')
+  let server
+  beforeEach(() => {
+    server = new Server(config)
+    return server.listenOnEndPoint()
+    .then(() => {
+      return server.start()
+    })
+  })
+
+  afterEach(() => {
+    return server.stop()
+  })
+
+  let resetReq = {
+    method: 'POST',
+    uri: `http://localhost:${config.server.port}/reset`,
+    form: {email: 'newsjianbo@163.com'},
+    followRedirect: false,
+    simple: false,
+    resolveWithFullResponse: true
+  }
+
+  it('should respond 401 if no email is matched', () => {
+    return request(resetReq)
+    .then((response) => {
+      assert.equal(response.statusCode, 401)
+    })
+  })
+  it.skip('should respond 200 if resetcode is sent to email', () => {
+    return server.createUser({email: 'newsjianbo@163.com', password: 'jianbo2017'})
+    .then(() => {
+      return server.activateUser('newsjianbo@163.com')
+    })
+    .then(() => {
+      return request(resetReq)
+    })
+    .then((response) => {
+      assert.equal(response.statusCode, 200)
+    })
+  })
 })
