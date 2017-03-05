3439cd5410261c85246521313c7c40af3acad852
Liu Shuo <liu@shuo.email>
2017-03-05 09:11:59 +0800

    use activation code as document id

--- src/Database/activation.js
+++ src/Database/activation.js
@@ -1,7 +1,7 @@
 var r = require('rethinkdb')
 
 function storeActivationCode (code, email) {
-  return r.db('SLRAT').table('activationcodes').insert({code, email}).run(this.connection)
+  return r.db('SLRAT').table('activationcodes').insert({id: code, email}).run(this.connection)
   .then((result) => {
     return Promise.resolve(true)
   })
@@ -12,20 +12,15 @@ function storeActivationCode (code, email) {
 
 function verifyActivation (code) {
   let email
-  return r.db('SLRAT').table('activationcodes').filter({code}).run(this.connection)
-  .then((result) => {
-    return result.next()
-  })
-  .then((row) => {
-    email = row.email
-    return r.db('SLRAT').table('activationcodes').get(row.id).delete().run(this.connection)
+  return r.db('SLRAT').table('activationcodes').get(code).run(this.connection)
+  .then((doc) => {
+    if (!doc) return Promise.reject(new Error('activation code not found'))
+    email = doc.email
+
+    return r.db('SLRAT').table('activationcodes').get(code).delete()
   })
   .then(() => {
     return Promise.resolve(email)
-  })
-  .catch((error) => {
-    if (error.msg === 'No more rows in the cursor.') return Promise.reject(new Error('activation code not found'))
-    throw new Error(error)
   })
 }
 
